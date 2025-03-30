be5d0493ad98ad34b4765688b5a6f54c04dde58e
eriol726 <erik.olsson627@gmail.com>
2025-03-30 12:56:17 +0200

    husky

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-register',
