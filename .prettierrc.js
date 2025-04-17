d2ac4825ac36e2350e9879a489194b468e200307
eriol726 <erik.olsson627@gmail.com>
2025-04-17 15:32:22 +0200

    importOrder config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,32 +1,44 @@
 module.exports = {
-    tabWidth: 4,
-    useTabs: false,
-    singleQuote: true,
-    semi: true,
-    bracketSpacing: true,
-    arrowParens: 'avoid',
-    trailingComma: 'es5',
-    bracketSameLine: true,
-    printWidth: 80,
-    endOfLine: 'auto',
-    overrides: [
-        {
-            files: '*.scss',
-            options: {
-                singleQuote: false,
-            },
-        },
-        {
-            files: '*.html',
-            options: {
-                printWidth: 120,
-            },
-        },
-        {
-            files: '*.js',
-            options: {
-                tabWidth: 2,
-            },
-        },
-    ],
+  tabWidth: 4,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  bracketSameLine: true,
+  printWidth: 80,
+  endOfLine: 'auto',
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrderParserPlugins: ['typescript', 'decorators-legacy'],
+  importOrder: [
+    '@angular',
+    'rxjs|ngx|@ngneat',
+    '<THIRD_PARTY_MODULES>',
+    '@app|@services|@interface|@modals',
+    '^[.]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  overrides: [
+    {
+      files: '*.scss',
+      options: {
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.html',
+      options: {
+        printWidth: 120,
+      },
+    },
+    {
+      files: '*.js',
+      options: {
+        tabWidth: 2,
+      },
+    },
+  ],
 };
