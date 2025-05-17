8079003743965e89fc8ab4bb64dc1f711d2edbe6
eriol726 <erik.olsson627@gmail.com>
2025-05-17 14:51:08 +0200

    renamed userService to userStoreService

--- src/app/services/user.store.service.spec.ts
+++ src/app/services/user.store.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserService } from './user.service';
+import { UserStoreService } from './user.store.service';
 
 describe('UserService', () => {
-    let service: UserService;
+    let service: UserStoreService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
-        service = TestBed.inject(UserService);
+        service = TestBed.inject(UserStoreService);
     });
 
     it('should be created', () => {
---
+++ src/app/services/user.store.service.ts
@@ -0,0 +1,69 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+
+import { Observable, ReplaySubject, tap } from 'rxjs';
+
+import { environment } from '@environments/environment.development';
+
+import { ApiResponse } from '@app/models/api-response';
+import {
+    loginResp,
+    menu,
+    RegisterConfirm,
+    ResetPasswordFormModel,
+    userCred,
+    UserRegistration,
+} from '@app/models/user.model';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserStoreService {
+    baseUrl = environment.apiUrl;
+
+    registerResponse = signal<RegisterConfirm>({
+        userid: 0,
+        username: '',
+        otptext: '',
+    });
+
+    _menulist = signal<menu[]>([]);
+    _username = signal<string>('');
+    showmenu: boolean = false;
+
+    private _menulist$ = new ReplaySubject<menu[]>(1);
+    public menulist$ = this._menulist$.asObservable();
+
+    constructor(private http: HttpClient) {}
+
+    UserRegistartion(_data: UserRegistration): Observable<ApiResponse> {
+        return this.http.post<ApiResponse>(`${this.baseUrl}User/userregistration`, _data);
+    }
+
+    ConfirmRegistartion(_data: RegisterConfirm): Observable<ApiResponse> {
+        return this.http.post<ApiResponse>(`${this.baseUrl}User/confirmregistration`, _data);
+    }
+
+    Proceedlogin(_data: userCred): Observable<loginResp> {
+        return this.http.post<loginResp>(`${this.baseUrl}Authorize/GenerateToken`, _data);
+    }
+
+    LoadMenuByRole(role: string): Observable<menu[]> {
+        console.log('load menu++');
+        return this.http.get<menu[]>(`${this.baseUrl}UserRole/GetAllMenusbyrole?userrole=${role}`).pipe(
+            tap(menuResponse => {
+                console.log('menuResponse: ', menuResponse);
+                this._menulist$.next(menuResponse);
+            })
+        );
+    }
+
+    Resetpassword(_data: ResetPasswordFormModel): Observable<ApiResponse> {
+        console.log('_data: ', _data);
+        return this.http.post<ApiResponse>(`${this.baseUrl}User/resetpassword`, _data);
+    }
+
+    Forgetpassword(username: string): Observable<ApiResponse> {
+        return this.http.get<ApiResponse>(`${this.baseUrl}User/forgetpassword?userName=${username}`);
+    }
+}
